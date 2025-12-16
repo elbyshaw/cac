60d55c0e5478a0c98ec511a519efc63b2a5ea377
Nick O <ndowens984@gmail.com>
2025-12-16 16:55:32 -0500

    cleanup

--- src/pkg.sv
+++ src/pkg.sv
@@ -2,6 +2,4 @@ package pkg;
     parameter NUM_BITS = 8;
 
     typedef enum logic [1:0] {S_PASSTHROUGH,S_LOAD,S_PROCESS} input_mux_t;
-    //TODO: Add any typedefs, enums, or parameters as neccessary for your project
-
 endpackage
