b076f4ae812bcf4dc8b50d34558dfd9a806804e8
Nick O <ndowens984@gmail.com>
2025-12-16 18:31:51 -0500

    added/finished accumulator.sv

--- src/accumulator.sv
+++ src/accumulator.sv
@@ -3,26 +3,29 @@ module pe import pkg::*;(
     input logic clk_i,
     input logic rst_i,
 
-    // DATA INPUTS / OUTPUTS
-    /* One row of elements in final C array;
-       takes N elements each NUM_BITS wide */
-    input logic [NUM_BITS-1:0] C_i [N],
-    output logic [NUM_BITS-1:0] product_0 [N][N], 
+    // DATA INPUT / OUTPUTS
+    // One element in final C array
+    input logic [NUM_BITS-1:0] C_i,
+    // N registers NUM_BITS wide
+    output logic [NUM_BITS-1:0] product_o [N],
 
     // CONTROL SIGNAL
-    
+    input logic valid_i
     );
 
-    int i,j;
+    int i;
 
     always_ff @(posedge clk_i) begin
         if (rst_i) begin
             for (i = 0; i < N; i++) 
-                for (j = 0; j < N; j++)
-                    product_o[i][j] <= '0;
+                product_o[i] <= '0;
         end else begin
-            for (i = 0; i < N; i++) 
-                
+            if (valid_i) begin
+                product_o[0] <= C_i;
+                for (i = 1; i < N; i++) begin
+                    product_o[i] <= product_o[i - 1];
+                end
+            end
         end
     end
 
