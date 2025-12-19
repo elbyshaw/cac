af960863d37b7db0e86b8feb0f4aa49b8f054914
cooper shaw <cooper@elbyshaw.com>
2025-12-19 14:44:09 -0500

    stage .sv files for transpilation

---
+++ src/pkg.sv
@@ -0,0 +1,12 @@
+package pkg;
+		parameter NUM_BITS = 8;
+
+    // represents dimension of arrays: N x N
+    parameter N = 4;
+
+    // number of bits needed to represent N elements
+    parameter LOG_N = 2;
+
+    typedef enum logic [1:0] {PASSTHROUGH, LOAD, PROCESS} input_mux_t;
+		typedef enum logic [1:0] {S_IDLE, S_LOADING, S_PROCESSING, S_END } state_t;
+endpackage
