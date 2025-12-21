b6972677e2497556e5b80894d78378597635555c
cooper shaw <cooper@elbyshaw.com>
2025-12-21 14:43:20 -0500

    add yosys opt project idea

--- sapr/synth.tcl
+++ sapr/synth.tcl
@@ -2,5 +2,16 @@ set src "../src/v"
 
 # import yosys commands as tcl commands
 yosys -import
-read_verilog -sv $src/pkg.sv $src/pe.sv
+
+# read design files
+read_verilog $src/*
+
+# set top level
+hierarchy -top top_lvl
+
+# write to RTL Intermediate Language, a yosys processing format
+write_rtlil
+
+# convert process (always blocks) to netlist elements and perform some simple optimizations
+
 
---
+++ subprojects/yosysopt/README.md
@@ -0,0 +1,1 @@
+the yosys command `opt` performs some "simple optimzations". can we make it better?
