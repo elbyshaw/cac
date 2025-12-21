4cf4095e1d41aab930c9cd9893c3d038b3624def
konstantingaydev <konstantingaydev16@gmail.com>
2025-12-21 12:40:05 -0800

    2nd tb case

--- sim/tb_systolic.sv
+++ sim/tb_systolic.sv
@@ -26,15 +26,15 @@ module tb_systolic ();
 	end
 
 	// generate reset
-	initial begin
-		rst = 1'b1;
-		#CLK_PERIOD
-		rst = 1'b0;
-	end
+	// initial begin
+	// 	rst = 1'b1;
+	// 	@(posedge clk);
+	// 	rst = 1'b0;
+	// end
 
 	// test stimulus
 	initial begin
-				/* Test 1: Identity multiply
+				/* 														Test 1: Identity multiply
 		   SIZE: N = 4
 		A =	{1 0 0 0
 			 0 1 0 0
@@ -51,57 +51,124 @@ module tb_systolic ();
 			 0 0 1 0
 			 0 0 0 1} */
 
+		rst = 1'b1;
+		@(posedge clk);
+		rst = 1'b0;
 		// LOADING PHASE
 		// cycle 0
 		in_north = '{0, 0, 0, 1'b1};
-		#CLK_PERIOD
+		@(posedge clk);
 		// cycle 1
 		in_north = '{0, 0, 1'b1, 0};
-		#CLK_PERIOD
+		@(posedge clk);
 		// cycle 2
 		in_north = '{0, 1'b1, 0, 0};
-		#CLK_PERIOD
+		@(posedge clk);
 		// cycle 3
 		in_north = '{1'b1, 0, 0, 0};
-		#CLK_PERIOD
+		@(posedge clk);
 
 		//PROCESSING PHASE
 		// cycle 4
 		in_west[0] = 1'b1;
-		#CLK_PERIOD
+		@(posedge clk);
 		// cycle 5
 		in_west[0] = 0;
 		in_west[1] = 0;
-		#CLK_PERIOD
+		@(posedge clk);
 		// cycle 6
 		in_west[0] = 0;
 		in_west[1] = 1;
 		in_west[2] = 0;
-		#CLK_PERIOD
+		@(posedge clk);
 		// cycle 7
 		in_west[0] = 0;
 		in_west[1] = 0;
 		in_west[2] = 0;
 		in_west[3] = 0;
-		#CLK_PERIOD
+		@(posedge clk);
 		// cycle 8
 		in_west[1] = 0;
 		in_west[2] = 1;
 		in_west[3] = 0;
-		#CLK_PERIOD
+		@(posedge clk);
 		// cycle 9
 		in_west[2] = 0;
 		in_west[3] = 0;
-		#CLK_PERIOD
+		@(posedge clk);
 		// cycle 10
 		in_west[3] = 1;
-		#CLK_PERIOD
+		@(posedge clk);
+		// cycle 11. You have to clear inputs after use bc of design of in_west?? otherwise itll keep inputting a value of 1.
+		in_west[3] = 0;
+		@(posedge clk);
+
+		repeat (5) @(posedge clk); // necessary for data to finish computing
+
+		repeat (5) @(posedge clk); // extra wait time
+
+		/*				
+		   																	Test 2 : All ones
+	
+		A =	{1 1 1 1
+			 1 1 1 1
+			 1 1 1 1
+			 1 1 1 1}
+
+		B =	{1 1 1 1
+			 1 1 1 1
+			 1 1 1 1
+			 1 1 1 1} 
+
+		C =	{4 4 4 4
+			 4 4 4 4
+			 4 4 4 4
+			 4 4 4 4} */
+
+		in_north = '{1'b1, 1'b1, 1'b1, 1'b1};
+		@(posedge clk);
+		// cycle 1
+		in_north = '{1'b1, 1'b1, 1'b1, 1'b1};
+		@(posedge clk);
+		// cycle 2
+		in_north = '{1'b1, 1'b1, 1'b1, 1'b1};
+		@(posedge clk);
+		// cycle 3
+		in_north = '{1'b1, 1'b1, 1'b1, 1'b1};
+		@(posedge clk);
+
+		//PROCESSING PHASE
+		// cycle 4
+		in_west[0] = 1'b1;
+		@(posedge clk);
+		// cycle 5
+		in_west[1] = 1;
+		@(posedge clk);
+		// cycle 6
+		in_west[2] = 1;
+		@(posedge clk);
+		// cycle 7.  At this point, all in_west inputs are 1. have to start clearing them out.
+		in_west[3] = 1;
+		@(posedge clk);
+		// cycle 8
+		in_west[0] = 0;
+		@(posedge clk);
+		// cycle 9
+		in_west[1] = 0;
+		@(posedge clk);
+		// cycle 10
+		in_west[2] = 0;
+		@(posedge clk);
 		// cycle 11
 		in_west[3] = 0;
-		#CLK_PERIOD
-		#(CLK_PERIOD * 5) // necessary for data to finish computing
+		@(posedge clk);
 
-		#(CLK_PERIOD * 5) // extra wait time
+		@(posedge clk);
+		repeat (5) @(posedge clk); // necessary for data to finish computing
+
+		repeat (5) @(posedge clk); // extra wait time
+
+
 
 		$finish;
 	end
